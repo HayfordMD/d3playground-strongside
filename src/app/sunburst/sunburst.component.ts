2e75e39b1c39ce35f8ebd0dea74fe01b9e329847
Mark Hayford <mdhayford@gmail.com>
2025-04-09 17:35:31 -0600

    this is done with deepseek didn't go well

---
+++ src/app/sunburst/sunburst.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sunburst',
+  imports: [],
+  template: `
+    <p>
+      sunburst works!
+    </p>
+  `,
+  styles: ``
+})
+export class SunburstComponent {
+
+}
